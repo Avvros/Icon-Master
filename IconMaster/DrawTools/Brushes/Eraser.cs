d923e0bd15c94927cecf56f39faa5815c8402f40
Alex Zharikov <alexzharikov34@gmail.com>
2021-06-22 10:56:29 +0300

    Added brushes subsystem

---
+++ IconMaster/DrawTools/Brushes/Eraser.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace IconMaster.DrawTools.Brushes
+{
+    class Eraser : Brush
+    {
+        public Eraser(WriteableBitmap bitmap, int bitmapWidth, int bitmapHeight) : base(bitmap, bitmapWidth, bitmapHeight)
+        {
+        }
+
+        protected override void DrawInternal(int x, int y)
+        {
+            bitmap.SetPixel(x, y, Colors.Transparent);
+        }
+    }
+}
---
+++ IconMaster/DrawTools/Brushes/Pen.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace IconMaster.DrawTools.Brushes
+{
+    class Pen : Brush
+    {
+        public Pen(WriteableBitmap bitmap, int bitmapWidth, int bitmapHeight) : base(bitmap, bitmapWidth, bitmapHeight)
+        {
+            Color = Colors.Black;
+        }
+
+        protected override void DrawInternal(int x, int y)
+        {
+            bitmap.SetPixel(x, y, Color);
+        }
+    }
+}
